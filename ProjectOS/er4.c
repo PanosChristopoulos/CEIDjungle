e1325e7f4366175fbd5dfb238590f317e8f75fb5
PanosChristopoulos <56166480+PanosChristopoulos@users.noreply.github.com>
2019-12-15 21:53:10 +0200

    Add files via upload

--- ProjectOS/er4.c
+++ ProjectOS/er4.c
@@ -1,53 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <time.h>
 #include <sys/wait.h>
 
-int dierg = 10;
+#define N 5000
 
-int counter = 0;
-
-void child() 
-{ //Δημιουργήσαμε την child function για να ελέγχεται ο ακριβής αριθμός των παιδιών και να καλείται η fork αναδρομικά
-    pid_t pid;
-    if (counter < dierg) {
-        counter++;
-        pid = fork();
-       if (pid < 0) {
-            printf("To fork apetyxe sth diergasia = %d\n", counter);
-        }
-        else if (pid == 0) {
-            printf("Dhmiourgithike to %d o paidi me pid %ld\n",counter, (long)getpid()); //το pid του παιδιού που δημιουργήθηκε
-            if (counter == dierg) {
-                exit(0);
-            }
-            else {
-                child();
-            }
-        }
-        else { //Παρουσιάζεται το ζητούμενο του ερωτήματος (pid διεργασίας, id παιδιού και id πατέρα)
-            long var  = pid;
-            int  ppid;
-            wait(&pid);
-            printf("To pid mou einai %ld eimai o pateras tou %ld child, o pateras mou einai %ld \n", (long)getpid(), var, (long)getppid());
-            exit(0);
-        }   
-    }
+int nothing(){
+	int x = 0;
+	x++;
+	return x;
 }
 
-int main(void) 
-{
-    pid_t pid;
-    pid = fork();
-    if (pid < 0) {
-        printf("To fork apetyxe %d\n", counter);
-        return -1;
-    }
-    if (pid == 0) {
-        child();
-    }
-    else {
-        wait(&pid);
-    }
-    return 0;
+int main(void){
+	time_t  begin,end;
+	time(&begin);
+	//clock_t start_t,end_t;
+	//start_t = clock();
+	pid_t pid[N],wpid;
+	int status;
+	//printf("Initial Seconds: %d \n",start_t);
+	printf("Initial Seconds: %d \n",begin);
+	int i = 0;
+	while(i <= N){
+		pid[i] = fork();
+		if(pid[i] == 0){
+			nothing();		
+			exit(0);		 		
+		}
+	i++;
+	}
+	
+	
+	while((wpid = wait(&status))>0);
+	time(&end);
+	//end_t = clock();
+	/*
+	for(i=0;i<N;i++){
+			
+		if(WIFEXITED(status)){
+			printf("Child %d terminated with exit status %d\n",wpid,WEXITSTATUS(status));
+		}else{
+			printf("Child %d terminated abnormally\n",wpid);	
+		}
+	}
+	*/
+	double total = (double)(end-begin)/(double)(N);
+	//double total = (double)(end_t-start_t)/CLOCKS_PER_SEC;
+	//printf("Final seconds value: %d\n",end_t);
+	printf("(end-start)/%d=%.20f \n",N,total);
+	return(0);
 }
