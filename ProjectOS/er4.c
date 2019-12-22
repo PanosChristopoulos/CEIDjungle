77def4e53573acf80324d94605a9a3407ec25845
PanosChristopoulos <56166480+PanosChristopoulos@users.noreply.github.com>
2019-12-23 00:28:22 +0200

    Add files via upload

---
+++ ProjectOS/er4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+
+#define N 5000
+
+int nothing(){
+	int x = 0;
+	x++;
+	return x;
+}
+
+int main(void){
+	time_t  begin,end;
+	time(&begin);
+	clock_t start_t,end_t;
+	start_t = clock();
+	pid_t pid[N],wpid;
+	int status;
+	printf("Έναρξη χρόνου %d \n",begin);
+	int i = 0;
+	while(i <= N){
+		pid[i] = fork();
+		if(pid[i] == 0){
+			nothing();		
+			exit(0);		 		
+		}
+	i++;
+	}
+	
+	
+	while((wpid = wait(&status))>0);
+	time(&end);
+	end_t = clock();
+
+	
+	double total = (double)(end_t-start_t)/CLOCKS_PER_SEC;
+	printf("Τελική ένδειξη ρολογιού %d\n",end_t);
+	printf("Χρόνος εκτέλεσης %d διεργασιών =%.20f \n",N,total);
+	return(0);
+}
